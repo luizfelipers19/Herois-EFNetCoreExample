4acc126aa287e21eedb354fffff6ec432d241431
LuizFelipe <luizfelipers19@gmail.com>
2021-05-16 20:37:43 -0300

    installed the Entity Framework nuget package

---
+++ EFCore-WebAPI/Data/HeroiContexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore_WebAPI.Data
+{
+    public class HeroiContexto : DbContext
+    {
+    }
+}
