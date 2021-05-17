23e595d37ad787e80e5ddcf7e54ccc69149e2d27
LuizFelipe <luizfelipers19@gmail.com>
2021-05-16 21:35:05 -0300

    applied the first migration and created the 3 tables: herois, armas, batalhas inside the MSSQL localhost

--- EFCore-WebAPI/Data/HeroiContexto.cs
+++ EFCore-WebAPI/Data/HeroiContexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,17 @@ namespace EFCore_WebAPI.Data
 {
     public class HeroiContexto : DbContext
     {
+        public DbSet<Heroi> Herois { get; set; }
+
+        public DbSet<Batalha> Batalhas { get; set;}
+
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = HeroApp; Integrated Security = True");
+        }
+
+
     }
 }
