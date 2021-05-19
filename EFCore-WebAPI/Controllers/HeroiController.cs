47a7bc31f949d7bcb8b87024b68061f08eadb443
LuizFelipe <luizfelipers19@gmail.com>
2021-05-19 09:24:56 -0300

    added the HeroiController class to make the intermediate process between http methods and requests

---
+++ EFCore-WebAPI/Controllers/HeroiController.cs
@@ -0,0 +1,66 @@
+﻿using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroiController : ControllerBase
+    {
+        private readonly HeroiContexto _context;
+
+        public HeroiController(HeroiContexto contexto)
+        {
+            _context = contexto;
+        }
+
+        // GET: api/<HeroiController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Erro: {ex}");
+            }
+
+
+            
+        }
+
+        // GET api/<HeroiController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/<HeroiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HeroiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HeroiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
